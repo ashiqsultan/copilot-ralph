30393c101388d0701cc5960d6b7c3447f4dfc188
AshiqSultan <ashiqdrive@gmail.com>
2026-01-29 14:45:25 +0530

    Added simple topbar

--- new_electron_react_app/copilot_ralph/src/renderer/src/App.jsx
+++ new_electron_react_app/copilot_ralph/src/renderer/src/App.jsx
@@ -1,5 +1,6 @@
 import Versions from './components/Versions'
 import electronLogo from './assets/electron.svg'
+import TopBar from './components/TopBar'
 
 function App() {
   const ipcHandle = () => window.electron.ipcRenderer.send('ping')
@@ -7,20 +8,15 @@ function App() {
   return (
     <>
       <div class="bg-gh-bg text-gh-text">
-        <h1 class="text-3xl font-bold ">Hello world!</h1>
-
-        <div id="topbar-container"></div>
+        <div id="topbar-container">
+          <TopBar />
+        </div>
 
         <div class="flex w-full">
           <div id="left-column" class="w-[70%]"></div>
 
           <div id="right-column" class="w-[30%]"></div>
         </div>
-      </div>
-      <div className="action">
-        <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
-          Send IPC
-        </a>
       </div>
     </>
   )
